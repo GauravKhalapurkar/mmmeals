e7f398bbb0a79d9cf3cb9a33a2220a1d6e67a955
Gaurav K <gaurav.khalapurkar@gmail.com>
2023-01-01 23:51:04 +0530

    categories_screen.dart done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './categories_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,22 +9,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'mmmeals',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('mmmeals'),
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        /*accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              titleLarge: TextStyle(
+                fontSize: 24,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),*/
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
-      ),
+      home: CategoriesScreen(),
     );
   }
 }
