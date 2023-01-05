ecb84f2743b76f05807a30da9a5b579315a1693a
Gaurav K <gaurav.khalapurkar@gmail.com>
2023-01-05 13:37:58 +0530

    Added pushNamed routes.

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+
+  // final String categoryId;
+  // final String categoryTitle;
+  // final Color color;
+  //
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle, this.color);
+
   @required
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryId = routeArgs['id'];
+    final categoryTitle = routeArgs['title'];
+    //final Color categoryColor = routeArgs['color'] as Color;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("The Recipes"),
+        title: Text(categoryTitle),
+        //backgroundColor: categoryColor,
       ),
       body: Center(
         child: Text(
